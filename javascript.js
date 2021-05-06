2756fe5a07417633b9edbf605895fba930e47f29
michaelhtran120 <michaelhtran120@gmail.com>
2021-05-06 11:41:30 -0700

    update javascript

--- javascript.js
+++ javascript.js
@@ -1,16 +1,87 @@
+// Style for container and total grid size
 const container = document.querySelector('#container');
-container.setAttribute('style', 'display: grid; grid-template-columns: repeat(16, 2em);')
+container.setAttribute('style', 
+    `display: inline-grid; 
+    grid-template-columns: repeat(16, 1fr);
+    grid-template-rows: repeat(16, 1fr);
+    width: 750px; 
+    height: 750px;`);
 
+//Style for body
 const body = document.querySelector('#body');
-body.setAttribute('style', 'display: flex; align-items: center; justify-content: center;')
+body.setAttribute('style', 
+    `display: flex; 
+    flex-direction: column;
+    width: 100vw;
+    align-items: center; 
+    justify-content: center;`)
 
-function createGrid() {
-    for (i=1; i<=256; i++) {
+// Row and column value for grid
+let value = 16;
+
+function createGrid(value) {
+    for (i=1; i<=value; i++) { //row
         const gridBox = document.createElement('div');
-        gridBox.setAttribute('style', 'background: lightgray; width: 2em; height: 2em; border: 0.5px solid black;');
+        gridBox.setAttribute('style', 
+            `background: lightgray; 
+            width: 1fr; 
+            height: 1fr; 
+            border: 0.1px solid grey;`);
+        gridBox.setAttribute('class', 'grid-box');
         container.appendChild(gridBox)
+
+        for (j=1; j<=value-1; j++){ //column
+            const gridBox = document.createElement('div');
+            gridBox.setAttribute('style', 
+                `background: lightgray; 
+                width: 1fr; 
+                height: 1fr; 
+                border: 0.1px solid grey;`);
+            gridBox.setAttribute('class', 'grid-box');
+            container.appendChild(gridBox)
+        }
+    } //Hover effect to change box color
+    document.querySelectorAll('.grid-box').forEach(box => {
+        box.addEventListener('mouseenter', Event => {
+            box.style.backgroundColor = 'black';
+        })
+    })
+}
+
+createGrid(value);
+
+// Create a button via javascript.
+const resetBtn = document.createElement('button');
+resetBtn.innerText="Reset Etch-A-Sketch";
+resetBtn.setAttribute('style', 
+    `padding: 5px 10px; 
+    margin-bottom: 2em; 
+    margin-top: 2em`)
+body.insertBefore(resetBtn, container);
+
+// Button function to reset grid.
+resetBtn.addEventListener('click', () => {
+    document.querySelectorAll('.grid-box').forEach(box => {
+        box.style.backgroundColor = 'lightgray';
+    })
+    promptNewGrid();
+    updateGrid();
+})
+
+// New Grid Value Prompt
+function promptNewGrid() {
+    value = prompt ('How many squares would you like for width? (height will have same squares)');
+    while (value < 5 || value > 100) {
+        value = prompt('Please choose a value between 5 and 100')
     }
 }
-createGrid();
-//const gridBox[i] = document.createElement('div');
-//gridBox.setAttribute('style', 'background: gray; width: 1em; height: 1em');
+
+//function to update grid
+function updateGrid() {
+    container.innerHTML= '';
+    container.style.setProperty('grid-template-columns', `repeat(${value}, 1fr)`);
+    container.style.setProperty('grid-template-rows', `repeat(${value}, 1fr)`);
+    createGrid(value);
+
+}
+
